1383c37e1297e154a6bbd3fc5b4eb81019162128
Dima Derevianko <dima@example.com>
2026-04-07 23:35:11 +0300

    Fix JUnit tests after adding OSType

--- src/test/java/ua/edu/sumdu/j2se/pr4/PhoneTest.java
+++ src/test/java/ua/edu/sumdu/j2se/pr4/PhoneTest.java
@@ -3,27 +3,15 @@ package ua.edu.sumdu.j2se.pr4;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Клас для тестування валідації класу Phone.
- */
 class PhoneTest {
-
     @Test
     void shouldThrowExceptionWhenInvalidValueInSetter() {
-        // Створюємо валідний об'єкт
-        Phone phone = new Phone("Apple", "iPhone 15", 999.0, 128, 3000);
-        
-        // Перевіряємо, чи вилетить помилка, якщо поставити від'ємну ціну
-        assertThrows(IllegalArgumentException.class, () -> {
-            phone.setPrice(-500.0);
-        });
+        Phone phone = new Phone("Apple", "iPhone 15", 999.0, 128, 3000, OSType.IOS);
+        assertThrows(IllegalArgumentException.class, () -> phone.setPrice(-500.0));
     }
 
     @Test
     void shouldThrowExceptionWhenInvalidConstructorData() {
-        // Перевіряємо, чи вилетить помилка при спробі створити телефон з пустим брендом
-        assertThrows(IllegalArgumentException.class, () -> {
-            new Phone("", "Galaxy", 500.0, 64, 4000);
-        });
+        assertThrows(IllegalArgumentException.class, () -> new Phone("", "Galaxy", 500.0, 64, 4000, OSType.ANDROID));
     }
 }
